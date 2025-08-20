ab90d90b428e830e9e1b2f43a489556608fb6709
Marcelo Rocha <marcelorocha.mfy@gmail.com>
2025-08-20 15:31:16 -0300

    first commit

---
+++ src/test/routes/editorasRoutes.test.js
@@ -0,0 +1,84 @@
+import {
+  afterEach, expect, it, jest,
+} from '@jest/globals';
+import request from 'supertest';
+import app from '../../app.js';
+
+let server;
+
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'applicatin/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let idResposta;
+describe('POST em barra editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const resposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'Sao Paulo',
+        email: 'c@c.com',
+      })
+      .expect(201);
+
+    idResposta = resposta.body.content.id;
+  });
+
+  it('Deve nao adicionar nada ao passar o body vazio', async () => {
+    await request(app)
+      .post('/editoras')
+      .send({})
+      .expect(400);
+  });
+});
+
+describe('GET em /editora/id', () => {
+  it('Deve retornar a editora selecionada', async () => {
+    await request(app)
+      .get(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
+
+describe('PUT em /editoras/id', () => {
+  test.each([
+    ['nome', { nome: 'Casa do Codigo' }],
+    ['cidade', { cidade: 'Sao Paulo' }],
+    ['email', { email: 'cdc@cdc.com' }],
+  ])('Deve alterar o campo %s', async (chave, param) => {
+    const requisicao = { request };
+    const spy = jest.spyOn(requisicao, 'request');
+    await requisicao.request(app)
+      .put(`/editoras/${idResposta}`)
+      .send(param)
+      .expect(204);
+
+    expect(spy).toHaveBeenCalled();
+  });
+});
+
+describe('DELETE em /editoras', () => {
+  it('Deletar o recurso adicionado', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
